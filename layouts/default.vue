6857e0f9158ec37af0276aa66bd010dc18a77934
Silue98 <94670819+Silue98@users.noreply.github.com>
2025-02-17 15:09:29 +0000

    mise à jour

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,7 @@
           class="flex items-center space-x-3 rounded-lg px-5 py-3 text-lg font-medium transition duration-300 bg-orange-100 hover:bg-orange-300 hover:text-white">
           <span>🏠</span>
           <span>Tableau de Bord</span>
+          <!---->
         </NuxtLink>
 
         <NuxtLink 
