0e2e290a1c26315e9a1f9bfa2fa390134d02a76e
Silue98 <94670819+Silue98@users.noreply.github.com>
2025-02-27 11:31:40 +0000

    mise à jour

--- pages/ListA1.vue
+++ pages/ListA1.vue
@@ -54,7 +54,7 @@
                 
                 <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                   <NuxtLink :to="`/PayementCard/${insc.Id_Incription}`"  class="bg-green-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Effectuer payement</NuxtLink>
-                  <!-- <NuxtLink :to="`/PdfPaiement/${insc.Id_Incription}`"  class="bg-green-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Voir</NuxtLink> -->
+                  <!--j <NuxtLink :to="`/PdfPaiement/${insc.Id_Incription}`"  class="bg-green-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Voir</NuxtLink> -->
                 </td>
                 
               </tr>
