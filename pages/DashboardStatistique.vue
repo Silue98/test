79f4e730026933f571612c8872a545f216177c10
Silue98 <94670819+Silue98@users.noreply.github.com>
2025-03-07 17:13:37 +0000

    rt

--- pages/DashboardStatistique.vue
+++ pages/DashboardStatistique.vue
@@ -1,6 +1,41 @@
 <template>
     <div class="p-6">
       <!-- Bouton Ajouter -->
+      <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mb-6">
+        <div class="bg-blue-500 text-white p-4 rounded-lg shadow-md flex justify-between items-center">
+          <div>
+            <h3 class="text-lg font-semibold">Total Enregistré</h3>
+            <p class="text-2xl">{{ enfant.length }}</p>
+          </div>
+          <i class="fas fa-users text-3xl"></i>
+        </div>
+  
+        <div class="bg-green-500 text-white p-4 rounded-lg shadow-md flex justify-between items-center">
+          <div>
+            <h3 class="text-lg font-semibold">Total Inscrits</h3>
+            <p class="text-2xl">{{ totalGarcons }}</p>
+          </div>
+          <i class="fas fa-male text-3xl"></i>
+        </div>
+  
+        <div class="bg-pink-500 text-white p-4 rounded-lg shadow-md flex justify-between items-center">
+          <div>
+            <h3 class="text-lg font-semibold">Total paiement</h3>
+            <p class="text-2xl">{{ totalFilles }}</p>
+          </div>
+          <i class="fas fa-female text-3xl"></i>
+        </div>
+  
+        <div class="bg-yellow-500 text-white p-4 rounded-lg shadow-md flex justify-between items-center">
+          <div>
+            <h3 class="text-lg font-semibold">Total Classe</h3>
+            <p class="text-2xl">{{ totalOrphelins }}</p>
+          </div>
+          <i class="fas fa-heart-broken text-3xl"></i>
+        </div>
+      </div>
+  
+
       <div class="flex">
         <button
           class="ml-auto mb-4 px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500"
@@ -188,3 +223,31 @@
   };
   </script>
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
