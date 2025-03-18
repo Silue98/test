1093e9bfce32bc46413e849333ba667324d0f6ce
Silue98 <94670819+Silue98@users.noreply.github.com>
2025-03-18 16:39:56 +0000

    read

--- pages/ListA4.vue
+++ pages/ListA4.vue
@@ -73,7 +73,7 @@
         <div class="flex flex-wrap items-center md:justify-between justify-center">
           <div class="w-full md:w-6/12 px-4 mx-auto text-center">
             <div class="text-sm text-blueGray-500 font-semibold py-1">
-              <!-- N'guessan <a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank">Fabrice</a> by <a href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> SMART DIGITAL</a>. -->
+              N'guessan <a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank">Fabrice</a> by <a href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> SMART DIGITAL</a>.
             </div>
           </div>
         </div>
